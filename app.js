fbeef0c9cc9cb38d5c774978e804038dc3d4c23c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-09-25 03:36:37 -0300

    Adicionado Javascript

---
+++ app.js
@@ -0,0 +1,48 @@
+const nav = document.querySelector('.nav');
+const btnMenu = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+
+
+function clickMenu(event) {
+  if(event.type === 'touchstart')
+    event.preventDefault();
+    event.stopPropagation();
+    nav.classList.toggle('active')
+  
+    clickForaDoMenu(menu, () => {
+    nav.classList.remove('active');
+    setAria();
+  })
+  setAria();
+}
+
+function clickForaDoMenu(elementoAlvo, callback) {
+  const html = document.documentElement;
+  
+  function handleHTMLClick(event) {
+    if(!elementoAlvo.contains(event.target)) {
+      elementoAlvo.removeAttribute('data-target');
+      html.removeEventListener('click', handleHTMLClick);
+      html.removeEventListener('touchstart', handleHTMLClick);
+      callback()
+    }
+  }
+  if(!elementoAlvo.hasAttribute('data-target')) {
+    html.addEventListener('click', handleHTMLClick);
+    html.addEventListener('touchstart', handleHTMLClick);
+    elementoAlvo.setAttribute('data-target', '');
+  }
+}
+
+function setAria() {
+  const estaAtivo = nav.classList.contains('active');
+  btnMenu.setAttribute('aria-expanded', estaAtivo);
+  if (estaAtivo) {
+    btnMenu.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    btnMenu.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMenu.addEventListener('click', clickMenu);
+btnMenu.addEventListener('touchstart', clickMenu);
